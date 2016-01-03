5a71c4459f661e6d547a97e9335aa9385d472d03
Felix Grayson <felixgrayson@hotmail.com>
2016-01-04 01:33:50 +1300

    AutoMapper added

---
+++ Application.Service/DBContext/AppUserDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System.Data.Entity;
+using GenericRepository;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Repository.Pattern.DataContext;
+using User.Data.Mapping;
+using User.Data.Model;
+
+namespace Application.Service.DBContext
+{
+	public class AppUserDbContext : DataContext
+	{
+		public AppUserDbContext()
+			: base("AppUserDbContext")
+		{
+		}
+
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<InventoryItem> InventoryItems { get; set; }
+		public DbSet<Labor> Labors { get; set; }
+		public DbSet<Part> Parts { get; set; }
+		public DbSet<ServiceItem> ServiceItems { get; set; }
+		public DbSet<WorkOrder> WorkOrders { get; set; }
+
+		public static AppUserDbContext Create()
+		{
+			return new AppUserDbContext();
+		}
+
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Configurations.Add(new CategoryConfiguration());
+			modelBuilder.Configurations.Add(new CustomerConfiguration());
+			modelBuilder.Configurations.Add(new InventoryConfiguration());
+			modelBuilder.Configurations.Add(new LaborConfiguration());
+			modelBuilder.Configurations.Add(new PartConfiguration());
+			modelBuilder.Configurations.Add(new ServiceItemConfiguration());
+			modelBuilder.Configurations.Add(new WorkOrderConfiguration());
+			modelBuilder.Configurations.Add(new AppUserConfiguration());
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
---
+++ GenericRepository/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Repository.Pattern.Infrastructure;
+
+namespace GenericRepository
+{
+    public abstract class EntityBase : IObjectState
+    {
+        [NotMapped]
+        public ObjectState ObjectState { get; set; }
+    }
+}
